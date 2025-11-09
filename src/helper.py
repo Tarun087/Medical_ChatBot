388499328dfa8220931a6a01d2f7a8657e6dc82b
Tarun087 <tamangtarun8@gmail.com>
2025-11-09 13:53:18 +0530

    new instance

--- src/helper.py
+++ src/helper.py
@@ -32,7 +32,7 @@ def  text_split(minimal_docs):
 
 
 
-
+##download embeddings
 def download_embeddings():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embeddings
